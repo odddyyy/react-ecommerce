2b7ec9cf93b2bf56e5c7a48b9128055112ef398b
odddyyy <56987696+odddyyy@users.noreply.github.com>
2020-05-03 14:34:47 +0700

    Initial commit

--- server/server.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config()
-const cors = require('cors')
-const express = require('express')
-const server = express()
-const PORT = process.env.PORT || 3001
-
-server.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
